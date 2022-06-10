e7b0d1fae5bc04fc83ab9bcdba2dea907773f8bc
issdn <isdnblk@gmail.com>
2022-06-10 18:16:28 +0200

    New facts.

--- line_counter/line_counter.py
+++ line_counter/line_counter.py
@@ -6,19 +6,28 @@ path = "..\\"
 ignore = [".git", "env", "__pycache__", ".vscode"]
 allowed_extensions = ["py", "js", "ts", "tsx", "ps1"]
 
+projects = [
+    directory
+    for directory in os.listdir(path)
+    if isdir(join(path, directory)) and directory not in ignore
+]
+
+
 files = []
 
 
-def tree(path=path):
+def file_tree(path=path):
     for item in os.listdir(path):
         pth = join(path, item)
         if isdir(pth) and item not in ignore:
-            tree(pth)
+            file_tree(pth)
         elif isfile(pth) and item.split(".")[1:][0] in allowed_extensions:
             files.append(pth)
 
 
-tree()
+file_tree()
+
+size = sum(os.path.getsize(file) for file in files)
 
 lines = sum(
     1
@@ -27,13 +36,21 @@ lines = sum(
     if line != "\n"
 )
 
+letters = sum(
+    1
+    for file in files
+    for letter in open(file, encoding="utf-8").read()
+    if letter != " " and letter != "\n"
+)
+
+
 readme = f"""
-👋 Hi, I'm Karol!\n
-👉 I have currently written **{lines}** lines in my **\\school-exercises** repository!\n
-📄 This was generated automatically and the code you can find in school-exercises.
+👋 Hi, I'm **Karol**!\n
+📌 I currently have **{len(projects)}** small projects in my *\\school-exercises* repository!\n
+🧩 That's **{len(files)}** files, **{lines}** lines, **{letters}** letters and **{size}** bytes of code!\n
+🤖 This was generated automatically by the code which you can find in *\\school-exercises\\line_counter*.
 """
 
-with open(md_path, "w+", encoding="utf-8") as md:
+with open(md_path, "w", encoding="utf-8") as md:
     md.write(readme)
     print("----README UPDATED----")
-    print(md.read())
